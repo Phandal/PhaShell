b9f1f100e1f75f076fd8c187cd23431c44eb667d
Phandal <bfeld.111@gmail.com>
2022-10-25 11:44:44 -0500

    Added Git Aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,40 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\customtheme.omp.json" | Invoke-Expression
 Set-PSReadLineOption -Colors @{ Command = '#ABB2BF' }
 
+function Git-Branch {
+  $branch = (git branch -a | fzf)
+  if ($branch) {
+      git checkout $branch.Trim()
+  }
+}
+
+function Git-Status {
+  git status
+}
+
+function Git-Diff {
+  git diff
+}
+
+function Git-Message-Commit {
+  param ([string]$Message)
+  if (!$Message){
+    Write-Host "You must include a message"
+    return
+  }
+  git commit -m $Message
+}
+
+function Git-Log-File {
+  git log
+}
+
+New-Alias gb Git-Branch
+New-Alias gs Git-Status
+New-Alias gd Git-Diff
+New-Alias gmc Git-Message-Commit
+New-Alias glf Git-Log-File
+
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
